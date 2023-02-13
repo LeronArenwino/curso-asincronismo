c9dd237fbaf82d187a027164aebe1d9348c4ecdb
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-13 16:17:36 -0500

    :zap: Generator

---
+++ src/generator/index.js
@@ -0,0 +1,23 @@
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const g = gen();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+function* iterate(array) {
+  for (const value of array) {
+    yield value;
+  }
+}
+const it = iterate(["Léron", "Francisco", "Ana", "Lucia", "Juan"]);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
